bd149f14fb2e51296cc0e19c3842af825a2dc843
jwafer77 <josh.wafer98@gmail.com>
2025-05-14 20:47:17 +1200

    Create Task9.java
    The basic code for this task

---
+++ Task9.java
@@ -0,0 +1,8 @@
+package Task9;
+
+public class Test {
+	public static void main(String[] args) {
+	System.out.println("This code is for task 9 of BIT504 Assessment 2");
+	System.out.println("Let's see what we add later!");
+	}
+}
